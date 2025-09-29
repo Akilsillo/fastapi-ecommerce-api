f2856bc7470428f80a3c574221cadc9198bb631a
Andres <avargasgut@gmail.com>
2025-09-29 14:12:27 -0300

    Basic configuration and project structure

---
+++ app/core/config.py
@@ -0,0 +1,41 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from typing import Annotated, Any, Literal
+
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    EmailStr,
+    HttpUrl,
+    PostgresDsn,
+    computed_field,
+    model_validator,
+)
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(
+        env_file='../.env'
+    )
+
+# Postres config
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+    
+
+settings = Settings()
